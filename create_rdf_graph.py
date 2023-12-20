999fe40145a798c28b36d2211a68eba55b10ec1d
yhutter <hutteryannick@gmail.com>
2023-12-20 19:19:05 +0100

    Query subjects for programming languages as well as use cases.

--- create_rdf_graph.py
+++ create_rdf_graph.py
@@ -13,7 +13,6 @@ def create_graph():
         "default": Namespace("http://www.fhgr.ch/ke-e/yhutter/so-programming-languages"),
         "wd": Namespace("http://www.wikidata.org/entity/"),
         "wdt": Namespace("http://www.wikidata.org/prop/direct/"),
-        "dpo": Namespace("https://dbpedia.org/")
     }
 
     # Bind all namespaces so that they do not need to be defined via PREFIX in queries...
@@ -21,6 +20,38 @@ def create_graph():
         g.bind(name, ns)
 
     return g
+
+def get_programming_language_subjects(programming_languages):
+    # Store a list with the found subject and the associated label
+    # e.g http://www.wikidata.org/entity/Q2005 => JavaScript
+    programming_language_subjects = dict()
+    for language in programming_languages:
+        query = PROGRAMMING_LANGUAGE_QUERY.replace("%programming_language%", language.lower())
+        result = g.query(query)
+        number_of_results = len(result)
+        if number_of_results == 0:
+            print(f"No result found for {language}, skipping...")
+            continue
+        elif number_of_results == 1:
+            print(f"Found exactly one match for {language}")
+            for query_language, query_label in result:
+                programming_language_subjects[query_label.strip()] = query_language
+        else:
+            # There have been multiple results choose the one that matches the language exactly.
+            print(f"Found multiple matches for {language}, trying to find the one that matches the description the closest.")
+            found_match = False
+            for query_language, query_label in result:
+                print(query_language, query_label)
+                # Do a simple match exactly by name and pick that one
+                if query_label.strip() == language:
+                    print(f"Found matching language {query_language} => {query_label}")
+                    programming_language_subjects[query_label] = query_language
+                    found_match = True
+                    break
+
+            if not found_match:
+                print(f"No matching language was found for {language}, skipping...")    
+    return programming_language_subjects
 
 if __name__ == "__main__":
     input_file = "./data/survey_results_public_cleaned.json"
@@ -31,27 +62,34 @@ if __name__ == "__main__":
     with open(input_file, "r") as f:
         survey_results = json.load(f)
 
-    programming_languages_people_have_worked_with = [
-        entry["language"] for entry in survey_results["languagesPeopleHaveWorkedWith"]]
+    # Extract languages which people have worked with.
+    languages_people_have_worked_with = [
+        entry["language"].strip() for entry in survey_results["languagesPeopleHaveWorkedWith"]]
+
+    # Preprocess languages (e.g remove '/', character and split into two)
+    preprocessed_languages_people_have_worked_with = []
+    for language in languages_people_have_worked_with:
+        preprocessed_languages_people_have_worked_with += language.split('/')
 
     g = create_graph()
 
-    programming_language_subjects = []
-    for language in programming_languages_people_have_worked_with[:1]:
-        query = PROGRAMMING_LANGUAGE_QUERY.replace("%programming_language%", language.lower())
+    programming_language_subjects = get_programming_language_subjects(preprocessed_languages_people_have_worked_with)
+    # programming_language_subjects = get_programming_language_subjects(["Python"])
+
+    print("Found the following subjects")
+    for label, subject in programming_language_subjects.items():
+        print(f"{label} => {subject}")
+
+    # Query for detail information using the found subjects
+    for language_label, subject in programming_language_subjects.items():
+        query = PROGRAMMING_LANGUAGE_HAS_USE_QUERY.replace("%programming_language_subject%", str(subject))
         result = g.query(query)
-        number_of_results = len(result)
-        if number_of_results == 0:
-            print(f"No result found for {language}, skipping...")
-            continue
-        elif number_of_results == 1:
-            print(f"Found exactly one match for {language}")
-            programming_language_subjects.append(result[0])
+        if len(result) == 0:
+            print(f"Found no use cases for language {language_label}")
         else:
-            # There have been multiple results try finding the result with the name that matches the closest.
-            # TODO: Try to find the one that matchest the closest.
-            print(f"Found multiple matches for {language}, trying to find the one that matches the description the closest.")
-            for query_language, query_label in result:
-                print(query_language, query_label)
-            
-       
+            has_use_literal_values = []
+            for has_use in result:
+                for literal in has_use:
+                    has_use_literal_values.append(literal.value)
+            print(f"Found the following use cases for language {language_label} => {', '.join(has_use_literal_values)}")
+
--- queries.py
+++ queries.py
@@ -1,18 +1,31 @@
 # %programming_language% functions as a template and will be dynamically replaced
 PROGRAMMING_LANGUAGE_QUERY = """
     SELECT DISTINCT ?programming_language ?programming_language_label WHERE {
-            SERVICE <https://query.wikidata.org/sparql> {
-              ?programming_language wdt:P31 ?instance_type;
-                                    rdfs:label ?programming_language_label.
+        SERVICE <https://query.wikidata.org/sparql> {
+          ?programming_language wdt:P31 ?instance_type;
+                                rdfs:label ?programming_language_label.
 
-              # instance of computer language
-              # The * goes up the inheritance hierarchy until it finds a match
-              ?instance_type wdt:P279* wd:Q629206.
+          # instance of computer language
+          # The * goes up the inheritance hierarchy until it finds a match
+          ?instance_type wdt:P279* wd:Q629206.
 
-              # For example CSS is called Cascading Style Sheets in english and CSS in german etc.
-              # Therefore we check both languages while also ignoring casing
-              FILTER((LANG(?programming_language_label) = "en" || LANG(?programming_language_label) = "de") && REGEX(str(?programming_language_label), "%programming_language%", "i"))
+          # For example CSS is called Cascading Style Sheets in english and CSS in german etc.
+          # Therefore we check both languages while also ignoring casing
+          FILTER((LANG(?programming_language_label) = "en" || LANG(?programming_language_label) = "de") && REGEX(str(?programming_language_label), "%programming_language%", "i"))
         }
     }
-    LIMIT 10
+    LIMIT 1000
+"""
+
+PROGRAMMING_LANGUAGE_HAS_USE_QUERY = """
+   SELECT DISTINCT ?has_use_label WHERE {
+        SERVICE <https://query.wikidata.org/sparql> {
+
+          <%programming_language_subject%> wdt:P366 ?has_use.
+          ?has_use rdfs:label ?has_use_label.
+  
+          FILTER(LANG(?has_use_label) = "en")
+        }
+    }
+    LIMIT 10 
 """
